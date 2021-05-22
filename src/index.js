8008851c0815b7c977d873c54c85528dfdd21f98
Daniel Komolafe <komozy2000@gmail.com>
2021-05-22 19:23:11 +0100

    Finished Jumbotron and added global styles

--- src/index.js
+++ src/index.js
@@ -1,9 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import 'normalize.css';
 import App from './app';
+import { GlobalStyles } from './global-styles';
 import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+    <>
+        <GlobalStyles />
+        <App />
+    </>,
+    document.getElementById('root')
+);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
