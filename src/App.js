f8ef7ebdb29caa059ec2d1c27f541298b8a17de1
Daniel Komolafe <komozy2000@gmail.com>
2021-06-19 11:19:43 +0100

    Restructuring

--- src/App.js
+++ src/App.js
@@ -1,24 +1,8 @@
 // import logo from './logo.svg';
-import JumboData from './fixtures/jumbo.json'
-import Jumbotron from './components/jumbotron'
+import { JumbotronContainer } from './containers/jumbotron';
 
 export default function App() {
   return (
-    <Jumbotron.Container>
-      {JumboData.map((item) => (
-        <Jumbotron key={item.id} direction={item.direction}>
-
-          <Jumbotron.Pane>
-            <Jumbotron.Title>{item.title}</Jumbotron.Title>
-            <Jumbotron.SubTitle>{item.subTitle}</Jumbotron.SubTitle>
-          </Jumbotron.Pane>
-
-          <Jumbotron.Pane>
-            <Jumbotron.Image src={item.image} alt={item.alt}></Jumbotron.Image>
-          </Jumbotron.Pane>
-
-        </Jumbotron>
-      ))}
-    </Jumbotron.Container>
+    <JumbotronContainer />
   );
 }
