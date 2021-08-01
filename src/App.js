7d9212375cbf03774761a6a9c440bda318ef24da
Daniel Komolafe <komozy2000@gmail.com>
2021-08-01 22:11:19 +0100

    Built the footer component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
+import FooterContainer from './containers/footer';
 // import logo from './logo.svg';
 import { JumbotronContainer } from './containers/jumbotron';
 
 export default function App() {
   return (
-    <JumbotronContainer />
+    <>
+      <JumbotronContainer />
+      <FooterContainer />
+    </>
   );
 }
